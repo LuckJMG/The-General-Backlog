983005000745e47466f94a96dd053737244cfe9f
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2024-09-14 12:56:02 -0300

    feat: edit entries

--- app.js
+++ app.js
@@ -106,9 +106,34 @@ function sortEntries(column, maintainOrder = false) {
 	entries.map(entry => insertToDashboard(entry));
 }
 
+function editEntry(entry) {
+	let entryId = entry.title.replace(" ", "_");
+	let entryIndex = entries.findIndex(a => a.title === entry.title);
+
+	let titleInput = document.getElementById("title_" + entryId);
+	let newTitle = titleInput.value;
+	if (newTitle !== entry.title && entries.some(a => a.title === newTitle))
+		return;
+	entries[entryIndex].title = titleInput.value;
+
+	let scoreInput = document.getElementById("score_" + entryId);
+	entries[entryIndex].score = parseInt(scoreInput.value);
+
+	let timeInput = document.getElementById("time_" + entryId);
+	entries[entryIndex].time = parseInt(timeInput.value);
+
+	entries[entryIndex].priority = entries[entryIndex].score / entries[entryIndex].time;
+
+	maxPriority = Math.max(...entries.map(entry => entry.priority));
+	minPriority = Math.min(...entries.map(entry => entry.priority));
+	minPriority = maxPriority === minPriority ? 0 : minPriority;
+	sortEntries(sortOrder.column, true);
+}
+
 function insertToDashboard(entry) {
 	let dashboard = document.getElementById("dashboard").children[1];
 	let row = dashboard.insertRow(1);
+	row.id = "entry_" + entry.title.replace(" ", "_");
 
 	let checkbox = document.createElement("input");
 	checkbox.type = "checkbox";
@@ -118,6 +143,7 @@ function insertToDashboard(entry) {
 
 	let titleCell = row.insertCell(1);
 	titleCell.innerHTML = entry.title;
+	titleCell.onclick = () => enableEditing(entry.title);
 
 	let scoreCell = row.insertCell(2);
 	scoreCell.innerHTML = entry.score;
@@ -129,6 +155,47 @@ function insertToDashboard(entry) {
 	let range = maxPriority - minPriority;
 	let scaledPriority = ((entry.priority - minPriority) / range) * SCALE;
 	priorityCell.innerHTML = Math.round(scaledPriority);
+}
+
+function enableEditing(title) {
+	sortEntries(sortOrder.column, true);
+	let entryId = title.replace(" ", "_");
+	let entry = entries.find(a => a.title === title);
+	let row = document.getElementById("entry_" + entryId);
+
+	let titleInput = document.createElement("input");
+	titleInput.type = "text";
+	titleInput.value = title;
+	titleInput.id = "title_" + entryId;
+	titleInput.required = true;
+	let titleCell = row.children[1];
+	titleCell.innerHTML = "";
+	titleCell.appendChild(titleInput);
+
+	let scoreInput = document.createElement("input");
+	scoreInput.type = "number";
+	scoreInput.value = entry.score;
+	scoreInput.id = "score_" + entryId;
+	scoreInput.required = true;
+	let scoreCell = row.children[2];
+	scoreCell.innerHTML = "";
+	scoreCell.appendChild(scoreInput);
+
+	let timeInput = document.createElement("input");
+	timeInput.type = "number";
+	timeInput.value = entry.time;
+	timeInput.id = "time_" + entryId;
+	timeInput.required = true;
+	let timeCell = row.children[3];
+	timeCell.innerHTML = "";
+	timeCell.appendChild(timeInput);
+
+	let acceptButton = document.createElement("button");
+	acceptButton.innerHTML = "Accept";
+	acceptButton.onclick = () => editEntry(entry);
+	let priorityCell = row.children[4];
+	priorityCell.innerHTML = "";
+	priorityCell.appendChild(acceptButton);
 }
 
 function showDeleteButton() {
