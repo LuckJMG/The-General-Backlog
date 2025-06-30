395db1bfe753cc66e783b0f997d63f109d9bd405
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2025-06-29 23:25:37 -0400

    feat: make navigation menu more pretty

--- src/lib/components/dashboard/DashboardControls.svelte
+++ src/lib/components/dashboard/DashboardControls.svelte
@@ -1,23 +1,65 @@
-<script>
-    import { Button } from "../ui/button";
+<script lang="ts">
+	import ChevronLeftIcon from "@lucide/svelte/icons/chevron-left";
+	import ChevronRightIcon from "@lucide/svelte/icons/chevron-right";
+	import { MediaQuery } from "svelte/reactivity";
+	import * as Pagination from "$lib/components/ui/pagination/index.js";
 
 	let { table } = $props();
+
+	let isDesktop = new MediaQuery("(min-width: 768px)");
+
+	let totalCount = $derived(table.getRowCount());
+	let pageSize = $derived(table.getState().pagination.pageSize);
+	let currentPageIndex = $derived(table.getState().pagination.pageIndex);
+	let currentPage = $derived(currentPageIndex + 1);
+	let totalPages = $derived(Math.ceil(totalCount / pageSize));
+
+	let siblingCount = $derived(isDesktop.current ? 1 : 0);
+
+	function handlePageChange(newPage: number) {
+		table.setPageIndex(newPage - 1);
+	}
 </script>
-<div class="flex items-center justify-end space-x-2 py-4">
-	<Button
-	variant="outline"
-	size="sm"
-	onclick={() => table.previousPage()}
-	disabled={!table.getCanPreviousPage()}
+
+<div class="flex items-center justify-end py-4">
+	<Pagination.Root
+		count={totalPages}
+		perPage={1}
+		{siblingCount}
+		page={currentPage}
+		onPageChange={handlePageChange}
 	>
-	Previous
-	</Button>
-	<Button
-	variant="outline"
-	size="sm"
-	onclick={() => table.nextPage()}
-	disabled={!table.getCanNextPage()}
-	>
-	Next
-	</Button>
+		{#snippet children({ pages, currentPage: paginationCurrentPage })}
+			<Pagination.Content>
+				<Pagination.Item>
+					<Pagination.PrevButton>
+						<ChevronLeftIcon class="size-4" />
+						<span class="hidden sm:block">Previous</span>
+					</Pagination.PrevButton>
+				</Pagination.Item>
+				{#each pages as page (page.key)}
+					{#if page.type === "ellipsis"}
+						<Pagination.Item>
+							<Pagination.Ellipsis />
+						</Pagination.Item>
+					{:else}
+						<Pagination.Item>
+							<Pagination.Link
+								{page}
+								isActive={paginationCurrentPage === page.value}
+							>
+								{page.value}
+							</Pagination.Link>
+						</Pagination.Item>
+					{/if}
+				{/each}
+				<Pagination.Item>
+					<Pagination.NextButton>
+						<span class="hidden sm:block">Next</span>
+						<ChevronRightIcon class="size-4" />
+					</Pagination.NextButton>
+				</Pagination.Item>
+			</Pagination.Content>
+		{/snippet}
+	</Pagination.Root>
 </div>
